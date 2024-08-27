7bdcea6290040dd9fc83916d3ae48455648a8cd4
iamwaleed07 <95676757+iamwaleed07@users.noreply.github.com>
2024-08-27 23:50:35 +0500

    db connect and user sign-up

--- src/actions/signup.ts
+++ src/actions/signup.ts
@@ -1,14 +1,28 @@
 "use server";
 
+import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/lib/db-utils";
 
 export const signup = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values)
+  const validatedFields = RegisterSchema.safeParse(values);
 
-    if (!validatedFields.success) {
-        return {error: "Invalid fields!"}
-    }
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
 
-    return {success: "Email sent!"}
-}
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) return { error: "User already exists" };
+
+  await db.user.create({
+    data: { name, email, password: hashedPassword },
+  });
+
+  return { success: "User created successfully" };
+};
