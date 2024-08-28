48ca83f4eaafb03328be532b76541055e252d4d1
waleed-vaival <waleed.ahmad@vaival.com>
2024-08-28 16:41:39 +0500

    signIn, signOut, and middleware for public and protected routes

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+export const publicRoutes = ["/"]
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/sign-up"
+]
+
+export const apiAuthPrefix = "/api/auth"
+
+/**
+ * The default redirect path after login
+ * @type { string }
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
